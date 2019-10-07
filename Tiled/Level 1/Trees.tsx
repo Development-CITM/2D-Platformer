304d085db6c268964df7a913a54d66c36e452fc6
[Eudald] <47557352+Hevne@users.noreply.github.com>
2019-10-07 10:38:42 +0200

    Added map + assets to folder

---
+++ Tiled/Level 1/Trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Trees" tilewidth="16" tileheight="16" tilecount="392" columns="28">
+ <image source="../Level 1 Assets/Trees pack/Trees.png" width="448" height="224"/>
+</tileset>
