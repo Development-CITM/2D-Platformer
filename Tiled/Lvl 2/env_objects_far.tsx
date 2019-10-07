4f6524e8ef5970cc7141af7bf51559fd6ca9d23b
Eudalet <47557352+Hevne@users.noreply.github.com>
2019-10-07 21:27:03 +0200

    Added Lvl 2 [Done] And player sprite sheet

--- Tiled/Lvl 2/env_objects_far.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="env_objects_far" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../Level 2/Castle/env_objects_far.png" width="512" height="512"/>
-</tileset>
--- Tiled/Lvl 2/middleground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="middleground" tilewidth="16" tileheight="16" tilecount="432" columns="24">
- <image source="../Level 2/Gothic/GothicVania-town-files/PNG/environment/layers/middleground.png" width="384" height="288"/>
-</tileset>
